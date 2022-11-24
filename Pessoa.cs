a2ec44b3a490c8b42505bd9e1f3fa80a68de6755
Medeiroshenrique <henrique.c.r.medeiros@gmail.com>
2022-11-24 14:57:57 -0300

    serializando

---
+++ Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializando
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, string sobrenome, int idade) {
+            if (nome!=""||sobrenome!=""||idade!=0) {
+                this.Nome = nome;
+                this.Sobrenome = sobrenome;
+                this.Idade = idade;
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Serializando;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+//CRIANDO LISTA PARA SERIALIZAR DEPOIS
+List<Pessoa> listaPessoa=new List<Pessoa>();
+Pessoa alguem1 = new Pessoa("Henrique","Medeiros",21);
+Pessoa jurema2 = new Pessoa("Jurema","Preta",87);
+Pessoa abraao3 = new Pessoa("Jucelino", "Catingueira", 100);
+listaPessoa.Add(alguem1);
+listaPessoa.Add(jurema2);
+listaPessoa.Add(abraao3);
+
+//SERIALIZANDO PARA JSON
+string meujson = JsonConvert.SerializeObject(listaPessoa, Formatting.Indented);
+File.WriteAllText("Arquivos/listaserializada.json",meujson);
+foreach (Pessoa pessoa in listaPessoa) {
+    Console.WriteLine($"Nome:{pessoa.Nome}\nSobrenome:{pessoa.Sobrenome}\nIdade:{pessoa.Idade}");
+    Console.WriteLine();
+}
